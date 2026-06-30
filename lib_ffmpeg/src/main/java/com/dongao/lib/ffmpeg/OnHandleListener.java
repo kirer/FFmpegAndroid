cb0b98042f35b2af2800d263ffd9a2e1750427af
xinwenbo <xinwenbo@dongao.com>
2026-06-30 10:19:08 +0800

    3333

--- lib_ffmpeg/src/main/java/com/dongao/lib/ffmpeg/OnHandleListener.java
+++
@@ -1,8 +0,0 @@
-package com.dongao.lib.ffmpeg;
-
-public interface OnHandleListener {
-    void onBegin();
-    void onEnd(int resultCode, String resultMsg);
-    void onProgress(int progress, int duration);
-    void onMsg(String msg);
-}
